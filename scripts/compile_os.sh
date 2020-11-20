d40488ccce962eb514418cb83c23907b70154842
xeno14 <integral14.dev@gmail.com>
2020-11-20 22:17:38 +0900

    Array.jack

--- scripts/compile_os.sh
+++ scripts/compile_os.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find os/*.jack | grep 'Math' | while read src; do
+find os/*.jack | grep 'Array' | while read src; do
     name=$(basename ${src/.jack//})
     dst_dir="projects/12/${name}Test/"
     tools/JackCompiler.sh $src
-    cp -v os/${name}.vm $dst_dir
+    mv -v os/${name}.vm $dst_dir
 done
