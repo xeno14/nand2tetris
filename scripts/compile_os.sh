78b336e4e633ef9d42049a41bbdd4fbdb8fe1383
xeno14 <integral14.dev@gmail.com>
2020-11-20 23:00:35 +0900

    String

--- scripts/compile_os.sh
+++ scripts/compile_os.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find os/*.jack | grep 'Array' | while read src; do
+find os/*.jack | grep 'String' | while read src; do
     name=$(basename ${src/.jack//})
     dst_dir="projects/12/${name}Test/"
     tools/JackCompiler.sh $src
