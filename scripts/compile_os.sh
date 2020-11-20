2ef9672d601f7a4944f06b9b143b261a517bd180
xeno14 <integral14.dev@gmail.com>
2020-11-20 18:55:34 +0900

    add files

--- scripts/compile_os.sh
+++ scripts/compile_os.sh
@@ -2,6 +2,7 @@
 
 find os/*.jack | while read src; do
     name=$(basename ${src/.jack//})
+    dst_dir="projects/12/${name}Test/"
     tools/JackCompiler.sh $src
-    cp -v os/${name}.vm projects/12/${name}Test/
+    cp -v os/${name}.vm $dst_dir
 done
