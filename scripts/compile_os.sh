c59d9bb6a5b780de1e470abaacac4b5f1dbfcd32
xeno14 <integral14.dev@gmail.com>
2020-11-21 13:05:57 +0900

    memory

--- scripts/compile_os.sh
+++ scripts/compile_os.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-find os/*.jack | grep 'Screen' | while read src; do
+find os/*.jack | grep 'Memory' | while read src; do
     name=$(basename ${src/.jack//})
     dst_dir="projects/12/${name}Test/"
     tools/JackCompiler.sh $src
